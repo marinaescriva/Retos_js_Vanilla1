36705d8abc81e4a50e0d49cafd108ce92b2ae974
Marina <marinaescriva.24@gmail.com>
2024-02-08 09:30:24 +0100

    Reto 2

--- main.js
+++ main.js
@@ -1,10 +1,21 @@
 // Retos js Vanilla 1
 
+
+
 // //1. Declara 2 variables numéricas (con el valor que desees), he indica cual es mayor de los
 // dos. Si son iguales indicarlo también. Ves cambiando los valores para comprobar que
 // funciona.
 
-let x = 5
-let y = 4
+// let x = 5
+// let y = 4
 
-resultado = (x>y) ? console.log("x es mayor que y") : ((x!=y) ? (console.log("y es mayor que x")): console.log("x e y son iguales"));
+// resultado = (x>y) ? console.log("x es mayor que y") : ((x!=y) ? (console.log("y es mayor que x")): console.log("x e y son iguales"));
+
+
+
+// 2. Declara un String que contenga tu nombre, después muestra un mensaje de bienvenida
+// por consola. Por ejemplo: si introduzco “Fernando”, me aparezca “Bienvenido Fernando”.
+
+let nombre ="Marina"
+
+console.log("Bienvenida" , nombre)
