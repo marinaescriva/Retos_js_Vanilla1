feaa51b2dbf26ddb0cd492954be96c5fab47dbcd
marinaescriva <marinaescriva.24@gamil.com>
2024-02-08 13:54:09 +0100

    Reto 6

--- main.js
+++ main.js
@@ -44,6 +44,17 @@
 // 5. Lee un número por teclado e indica si es divisible entre 2 (resto = 0). Si no lo es, también
 // debemos indicarlo.
 
-let numero = parseInt(prompt("dime un numero"));
+// let numero = parseInt(prompt("dime un numero"));
 
-((numero % 2) == 0) ? console.log("es divisible por 2") : console.log ("no es divisible por 2");
+// ((numero % 2) == 0) ? console.log("es divisible por 2") : console.log ("no es divisible por 2");
+
+
+// 6. Lee un número por teclado que pida el precio de un producto (puede tener decimales) y
+// calcule el precio final con IVA. El IVA será una constante que sera del 21%.
+
+let precio = parseInt(prompt("¿Qué precio tiene x?"))
+const iva = 0.21;
+
+let precioFinal = precio + (precio * iva);
+
+console.log(precioFinal);
