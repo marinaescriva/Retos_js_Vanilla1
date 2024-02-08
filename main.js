abc74ac15bbb6e6e9ba71b75d060f8d026860898
marinaescriva <marinaescriva.24@gamil.com>
2024-02-08 09:18:11 +0100

    Reto 1

---
+++ main.js
@@ -0,0 +1,10 @@
+// Retos js Vanilla 1
+
+// //1. Declara 2 variables numéricas (con el valor que desees), he indica cual es mayor de los
+// dos. Si son iguales indicarlo también. Ves cambiando los valores para comprobar que
+// funciona.
+
+let x = 5
+let y = 4
+
+resultado = (x>y) ? console.log("x es mayor que y") : ((x!=y) ? (console.log("y es mayor que x")): console.log("x e y son iguales"));
